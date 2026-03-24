8c447451c5ab942fe9b89f4358d94a395c7bef7d
Rao Muhammad Dayan Atif <raodayanatif@gmail.com>
2026-03-24 10:02:37 +0500

    deploy

--- rotate_docker_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "========== Started Docker Log Rotation =========="
-date
-
-sudo find /var/lib/docker/containers/ -type f -name "*.log" -exec truncate -s 0 {} \;
-
-echo "Successfully truncated Docker log files."
-echo "========== Finished Docker Log Rotation =========="
